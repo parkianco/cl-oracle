ec1eaf94741c47abc13c3f0c4b0778e75e2345d6
Park Ian Co <opensource@parkian.co>
2026-03-15 00:12:22 -0400

    Relicense from BSD-3-Clause to Apache-2.0

--- src/oracle.lisp
+++ src/oracle.lisp
@@ -1,5 +1,5 @@
 ;;;; Copyright (c) 2024-2026 Parkian Company LLC. All rights reserved.
-;;;; SPDX-License-Identifier: BSD-3-Clause
+;;;; SPDX-License-Identifier: Apache-2.0
 
 ;;;; cl-oracle/oracle.lisp - Main oracle interface
 ;;;; High-level API for price oracle operations
